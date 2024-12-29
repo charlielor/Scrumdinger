3bc2c1b554cfdcf5674d59452f3b6e55ad701b3a
Charlie Lor <lorcharlie@gmail.com>
2024-12-29 17:46:12 -0600

    Initial Commit

---
+++ ScrumdingerTests/ScrumdingerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerTests.swift
+//  ScrumdingerTests
+//
+//  Created by Charlie Lor on 12/29/24.
+//
+
+import Testing
+@testable import Scrumdinger
+
+struct ScrumdingerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
